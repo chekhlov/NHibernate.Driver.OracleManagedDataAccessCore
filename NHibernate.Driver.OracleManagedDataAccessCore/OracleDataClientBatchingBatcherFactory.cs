bc831a294ab11bf304b5d45d5c36c994f44fbdf7
Alexander Chekhlov <chekhlov.alexander@gmail.com>
2019-06-23 16:40:15 +0300

    Create project

---
+++ NHibernate.Driver.OracleManagedDataAccessCore/OracleDataClientBatchingBatcherFactory.cs
@@ -0,0 +1,13 @@
+﻿using NHibernate.AdoNet;
+using NHibernate.Engine;
+
+namespace NHibernate.Driver.OracleManagedDataAccessCore
+{
+    public class OracleDataClientBatchingBatcherFactory : IBatcherFactory
+    {
+        public virtual IBatcher CreateBatcher(ConnectionManager connectionManager, IInterceptor interceptor)
+        {
+            return new OracleDataClientBatchingBatcher(connectionManager, interceptor);
+        }
+    }
+}
